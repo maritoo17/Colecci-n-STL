9ec71229d69b1e1588a610fb6e5d214c190f8ed1
maritoo17 <msancgim@myuax.com>
2023-11-15 17:22:54 +0100

    Final del ejercicio en el que creo un cuadrado para acabar.

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -3,6 +3,7 @@
 #include "circulo.cpp"
 #include "pentagono.cpp"
 #include "decagono.cpp"
+#include "cuadrado.cpp"
 
 int main() {
     double baseTriangulo = 5.0;
@@ -11,11 +12,13 @@ int main() {
     double ladoPentagono = 5.0;
     double apotemaPentagono = 3.0;
     double ladoDecagono = 8.0;
+    double ladoCuadrado = 4.0;
 
     std::cout << "Área del triángulo: " << Geometria::calcularAreaTriangulo(baseTriangulo, alturaTriangulo) << std::endl;
     std::cout << "Área del círculo: " << Geometria::calcularAreaCirculo(radioCirculo) << std::endl;
     std::cout << "Área del pentágono: " << Geometria::calcularAreaPentagono(ladoPentagono, apotemaPentagono) << std::endl;
     std::cout << "Área del decágono: " << Geometria::calcularAreaDecagono(ladoDecagono) << std::endl;
+    std::cout << "Área del cuadrado: " << Geometria::calcularAreaCuadrado(ladoCuadrado) << std::endl;
 
     return 0;
 }
