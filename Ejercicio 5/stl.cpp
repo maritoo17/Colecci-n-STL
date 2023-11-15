c6f939438a64267652704c5885cfdb21b216d6fd
maritoo17 <msancgim@myuax.com>
2023-11-15 18:17:42 +0100

    Final del último ejercicio en el que te piden aplicar tres diferentes contenedores de stl.

---
+++ Ejercicio 5/stl.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int main() {
+    vector <int> vector1;
+
+    vector1.push_back(10);
+    vector1.push_back(20);
+    vector1.push_back(30);
+
+    cout << "Elementos del vector: " << endl;
+    for(int elemento : vector1) {
+        cout << elemento << endl;
+    }
+    cout << endl;
+
+    std::map<int, string> mapa;
+
+    mapa[1] = "uno";
+    mapa[2] = "dos";
+    mapa[3] = "tres";
+
+    cout << "Elementos del mapa: " << endl;
+    for(const auto& par : mapa) {
+        cout << par.first << " -> " << par.second << endl;
+    }
+
+    set<int> Set;
+
+    Set.insert(10);
+    Set.insert(20);
+    Set.insert(30);
+
+    cout << "Elementos del set: " << endl;
+    for(int elemento : Set) {
+        cout << elemento << " ";
+    }
+    cout << endl;
+    return 0;
+
+
+
+
+
+}
