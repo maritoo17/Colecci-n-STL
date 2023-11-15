971d79be28aac8cb96df44e01003cb4491bd594d
maritoo17 <msancgim@myuax.com>
2023-11-15 16:56:00 +0100

    Segundo cambio en el qye añado unas operaciones básicas más.

--- Ejercicio 1/nombres.cpp
+++ Ejercicio 1/nombres.cpp
@@ -7,10 +7,24 @@ namespace Matematicas {
     int resta(int a, int b) {
         return a - b;
     }
+    int multiplicacion(int a, int b) {
+        return a * b;
+    }
+    int division(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        }
+    }
+    int cuadrado(int a) {
+        return a * a;
+    }
 }
 
 int main() {
     std::cout << "Suma: " << Matematicas::suma(5, 3) << std::endl;
     std::cout << "Resta: " << Matematicas::resta(5, 3) << std::endl;
+    std::cout << "Multiplicacion: " << Matematicas::multiplicacion(5, 3) << std::endl;
+    std::cout << "Division: " << Matematicas::division(6, 2) << std::endl;
+    std::cout << "Cuadrado de 5: " << Matematicas::cuadrado(5) << std::endl;
     return 0;
 }
