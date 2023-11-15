2c7f9a1d31f45de1e24dfd3d432d9e9c16189690
maritoo17 <msancgim@myuax.com>
2023-11-15 17:19:16 +0100

    Tercera parte del ejercicio en el que añado un decagono y arreglo el codigo con ayud de chat gpt para que no me salga un error.

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -2,15 +2,21 @@
 #include "triangulo.cpp"
 #include "circulo.cpp"
 #include "pentagono.cpp"
+#include "decagono.cpp"
 
 int main() {
-    double base = 5.0;
-    double altura = 3.0;
-    double radio = 2.5;
+    double baseTriangulo = 5.0;
+    double alturaTriangulo = 3.0;
+    double radioCirculo = 2.5;
+    double ladoPentagono = 5.0;
+    double apotemaPentagono = 3.0;
+    double ladoDecagono = 8.0;
 
-    std::cout << "Área del triángulo: " << Geometria::calcularAreaTriangulo(base, altura) << std::endl;
-    std::cout << "Área del círculo: " << Geometria::calcularAreaCirculo(radio) << std::endl;
-    std::cout << "Área del pentágono: " << Geometria::calcularAreaPentagono(base, altura) << std::endl;
+    std::cout << "Área del triángulo: " << Geometria::calcularAreaTriangulo(baseTriangulo, alturaTriangulo) << std::endl;
+    std::cout << "Área del círculo: " << Geometria::calcularAreaCirculo(radioCirculo) << std::endl;
+    std::cout << "Área del pentágono: " << Geometria::calcularAreaPentagono(ladoPentagono, apotemaPentagono) << std::endl;
+    std::cout << "Área del decágono: " << Geometria::calcularAreaDecagono(ladoDecagono) << std::endl;
 
     return 0;
 }
+
