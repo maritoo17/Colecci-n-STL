c84bd75b81882b84220b5972b0ca4edd08832e6c
maritoo17 <msancgim@myuax.com>
2023-11-15 17:14:34 +0100

    Segunda parte en la que añado un pentagono.

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "triangulo.cpp"
 #include "circulo.cpp"
+#include "pentagono.cpp"
 
 int main() {
     double base = 5.0;
@@ -9,6 +10,7 @@ int main() {
 
     std::cout << "Área del triángulo: " << Geometria::calcularAreaTriangulo(base, altura) << std::endl;
     std::cout << "Área del círculo: " << Geometria::calcularAreaCirculo(radio) << std::endl;
+    std::cout << "Área del pentágono: " << Geometria::calcularAreaPentagono(base, altura) << std::endl;
 
     return 0;
 }
